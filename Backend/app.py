73f119689bc344be88c84f8ee2116c22df1ec6e1
Timoh-Mbata <timothymbata450@gmail.com>
2026-02-15 00:02:17 +0300

    v1 of the backend

--- Backend/app.py
+++ Backend/app.py
@@ -1,9 +1,9 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Body
 from .predict import Predict as pred
 
 router = APIRouter()
 
 @router.post("/predict-api")
-async def prediction_requests(x_features):
-    model_results = pred(X_features=x_features)
+async def prediction_requests(x_features = Body(...)):
+    model_results = await pred(X_features=x_features)
     return model_results
