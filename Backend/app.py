729121305d6738fe3fedf279760f36de7f599172
Timoh-Mbata <timothymbata450@gmail.com>
2026-02-14 23:40:08 +0300

    Worked on the api

--- Backend/app.py
+++ Backend/app.py
@@ -1,1 +1,9 @@
-print("...DSSAI Backend Starting...")
+from fastapi import APIRouter
+from .predict import Predict as pred
+
+router = APIRouter()
+
+@router.post("/predict-api")
+async def prediction_requests(x_features):
+    model_results = pred(X_features=x_features)
+    return model_results
